5a83961aa33dd738b6b585a90967a3a12346be5d
Severin Hanke <severin.hanke@vomatec.de>
2023-06-27 17:02:08 +0200

    Started game development

--- lib/ui/levels/level_1.dart
+++ lib/ui/levels/level_1.dart
@@ -1,15 +1,21 @@
-import 'dart:ui';
+import 'dart:async';
 
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/palette.dart';
+import 'package:flutter/material.dart';
 
-class Level1 extends Game {
+class Level1 extends FlameGame {
   @override
-  void render(Canvas canvas) {
-    // TODO: implement render
-  }
-
-  @override
-  void update(double dt) {
-    // TODO: implement update
+  FutureOr<void> onLoad() async {
+    print(size);
+    print(size.length2);
+    addAll([
+      RectangleComponent(size: Vector2(5, size.y), position: Vector2(0, 0), paint: BasicPalette.brown.paint()),
+      RectangleComponent(size: Vector2(size.x, 5), position: Vector2(0, 0), paint: BasicPalette.brown.paint()),
+      RectangleComponent(size: Vector2(5, size.y), position: size, anchor: Anchor.bottomRight, paint: BasicPalette.brown.paint()),
+      RectangleComponent(size: Vector2(size.x, 5), position: size, anchor: Anchor.bottomRight, paint: BasicPalette.brown.paint()),
+      CircleComponent(radius: 25, anchor: Anchor.center, position: size / 2),
+    ]);
   }
 }
